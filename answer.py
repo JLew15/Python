b47c6fff9680c723457953b7c742f6ff32fdc0fc
Jaiden Lewis <jaiden.lewis@tooeleschools.org>
2020-09-09 08:51:18 -0600

    edited answer.py

--- answer.py
+++ answer.py
@@ -2,3 +2,13 @@ x = 10
 y = 5
 answer = x+y
 print(answer)
+answer2 = x*y
+print(answer2)
+answer3 = y**x
+print(answer3)
+answer4 = x/y
+print(answer4)
+answer5 = x-y
+print(answer5)
+answer6 = answer*answer2*answer3*answer4*answer5
+print(answer6)
