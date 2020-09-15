7a3ce9f72bfd3ff33540cb0c26bd2928d94028da
JLew15 <zombears@icloud.com>
2020-09-15 10:11:14 -0600

    afdsafdsafsadfsadfdsaf

--- chapter2.1examples.py
+++ chapter2.1examples.py
@@ -6,3 +6,24 @@ name = "Jay"
 print(type(name))
 awake = True
 print(type(awake))
+
+#Key words
+#True
+#False
+#and
+#as
+#assert
+#break
+#class
+#continue
+#def
+#del
+#elif
+#else
+#except
+#finally
+#for
+#from
+#global
+#if
+#import
