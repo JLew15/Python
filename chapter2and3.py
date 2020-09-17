d65d0f015f56ad41cd93ff7195fa023a3ca66e59
JLew15 <zombears@icloud.com>
2020-09-17 10:17:03 -0600

    jhgjhgjhgjhgjhgjhgjhgjhg

---
+++ chapter2and3.py
@@ -0,0 +1,21 @@
+number = 10
+print(type(number))
+score = 10000
+number2 = 7.0
+answer = number + number2
+print(type(answer))
+print(answer)
+
+print(2+5*7/8+3/(8+7*4))
+tableline="----------------------------------"
+strScore = "Score:-------------------- " + str( score + 100) + "\n" + tableline
+print(strScore)
+print(strScore)
+print(strScore)
+print(strScore)
+print(strScore)
+print(strScore)
+print(strScore)
+print(strScore)
+print(strScore)
+print(strScore)
---
+++ highscorelist.py
@@ -0,0 +1,58 @@
+# create and initialize three name variables
+# (name1, name2, name3)
+name1 = "BossLady"
+name2 = "BossMan"
+name3 = "Screams"
+name4 = "Doctor"
+name5 = "ReeKid"
+name6 = "RandomMan"
+name7 = "RandomKid"
+name8 = "RandomLady"
+name9 = "RandomRunner"
+name10 = "Player222"
+
+
+
+# create and initialize three score variables
+# (score1, score2, score3)
+score1 = 1000
+score2 = 995
+score3 = 990
+score4 = 985
+score5 = 980
+score6 = 975
+score7 = 970
+score8 = 965
+score9 = 960
+score10 = 400
+
+
+
+# print a title for the high scores list
+print("      High Scores")
+print("----------------------")
+
+# Line between each of the scores.
+spacerline = "----------------------"
+
+# print each of the three score lines in the pattern "name = score"
+print(name1 + " = " + str(score1))
+print(spacerline)
+print(name2  + " = "  + str(score2))
+print(spacerline)
+print(name3  + " = "  + str(score3))
+print(spacerline)
+print(name4  + " = "  + str(score4))
+print(spacerline)
+print(name5  + " = "  + str(score5))
+print(spacerline)
+print(name6  + " = "  + str(score6))
+print(spacerline)
+print(name7  + " = "  + str(score7))
+print(spacerline)
+print(name8  + " = "  + str(score8))
+print(spacerline)
+print(name9  + " = "  + str(score9))
+print(spacerline)
+print(name10  + " = "  + str(score10))
+print(spacerline)
