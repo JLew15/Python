6796520b2f3481a9fdfc8bb6c3827bfb769555d6
JLew15 <zombears@icloud.com>
2020-09-21 08:45:43 -0600

    kdsajfjdsajfjkdsajlkgfjdksjflkjdsag9376987 6548768743r87fujhdfguyer65tr8743u6 58 4387548698547654987698754986754987698547986

---
+++ chapter3examples.py
@@ -0,0 +1,12 @@
+testResults = "Pass"
+
+#print("Testing 1,2,3" + " "+testResults)
+
+score = 100
+print(type(score))
+print("Score:",score,sep=",",end=" ")
+print("Score:",score,sep=",",end="\n")
+print("Score:",score,sep=",",end=" ")
+print("Score:",score,sep=",",end=" ")
+print("Score:",score,sep=",",end="a")
+print("Score:",score,sep=",",end="b")
