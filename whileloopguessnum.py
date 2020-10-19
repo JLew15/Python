ed40b59bf2bc72b2c11f9ab16b76b18448f771fc
Jaiden Lewis <Zombears@icloud.com>
2020-10-19 07:34:22 -0600

    AA

--- whileloopguessnum.py
+++ whileloopguessnum.py
@@ -1,15 +1,35 @@
 import random
 
-theNumber = random.randint(1,10)
+
 
 win = False
 attempts = 1
 maxAttempts = 3
 maxNumber = 10
+diff = 1
+
+difficulty = input("Choose a difficulty... Easy, Medium, or Hard. ")
+
+if difficulty == "Easy" or difficulty.startswith("E") or difficulty.startswith("e"):
+    maxAttempts = 3
+    maxNumber = 10
+    diff = 1
+
+elif difficulty == "Medium" or difficulty.startswith("M") or difficulty.startswith("m"):
+    maxAttempts = 5
+    maxNumber = 50
+    diff = 2
+
+else:
+    maxAttempts = 10
+    maxNumber = 100
+    diff = 3
 
 print("\tWelcome to 'Guess My Number'!")
 print("\nI'm thinking of a number between 1 and",maxNumber)
 print("Try to guess it in", maxAttempts, " attempts.\n")
+
+theNumber = random.randint(1,maxNumber)
 
 while attempts <= maxAttempts:
     if not win:
