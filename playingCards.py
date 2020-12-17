6e276413c8c5fb33d167dab2f05d9222856731a9
JLew15 <zombears@icloud.com>
2020-12-17 09:29:07 -0700

    dskajflkdsajf

--- playingCards.py
+++ playingCards.py
@@ -1,5 +1,34 @@
-class Card(object):
+import random
 
-    def __init__(self):
-        self.suit = ""
-        self.rank = ""
+class Card(object):
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS =["♥", "♦", "♣", "♠"]
+
+    def __init__(self, rank, suit):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        rep = str.format("""
+        +--------+
+        | {1}{0:2}    |
+        |        |
+        |        |
+        |        |
+        |        |
+        |        |
+        |    {0:2}{1} |
+        +--------+
+        """,self.rank,self.suit)
+        return rep
+
+
+
+hand = []
+for i in range(5):
+    suit = random.choice(Card.SUITS)
+    rank = random.choice(Card.RANKS)
+    card = Card(rank,suit)
+    hand.append(card)
+for card in hand:
+    print(card)
