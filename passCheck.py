c1855ad66e726d6f0f830a66c3816d80ca665f5f
JLew15 <zombears@icloud.com>
2021-02-04 11:41:09 -0700

    8199 Thu 02/04/2021 11:41

---
+++ passCheck.py
@@ -0,0 +1,8 @@
+password = "https://www.google.com/search?client=firefox-b-1-d&q=catholics"
+while True:
+    passCode = input("Enter password.")
+
+    if passCode == password:
+        print("You got in.")
+    else:
+        print("Failure.")
