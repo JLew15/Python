63cef759fdb6517c95a2255fcf92e98e05dee59f
JLew15 <zombears@icloud.com>
2021-03-04 10:08:13 -0700

    20883 Thu 03/04/2021 10:08

--- PyGameTemplate/main.py
+++ PyGameTemplate/main.py
@@ -1,6 +1,13 @@
 import math
 import random
 import pygame
+import os
+
+
+gameFolder = os.path.dirname(__file__)
+imgFolder = os.path.join(gameFolder, "img")
+audFolder = os.path.join(gameFolder, "aud")
+mouseHeld = False
 
 class NPC(pygame.sprite.Sprite):
     def __init__(self):
@@ -8,20 +15,20 @@ class NPC(pygame.sprite.Sprite):
         self.image = pygame.Surface((25, 25))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.speedX = 5
         self.speedY = 6
         self.angle = 0
         self.doCircle = False
 
     def update(self):
-        if self.rect.right >= WIDTH-1 or self.rect.left <= 1:
+        if self.rect.right >= WIDTH - 1 or self.rect.left <= 1:
             self.speedX = -self.speedX
             if self.speedX < 0:
                 self.speedX -= 1
             else:
                 self.speedX += 1
-        if self.rect.bottom >= HEIGHT-1 or self.rect.top <=1:
+        if self.rect.bottom >= HEIGHT - 1 or self.rect.top <= 1:
             self.speedY = -self.speedY
             if self.speedY < 0:
                 self.speedY -= 1
@@ -30,19 +37,30 @@ class NPC(pygame.sprite.Sprite):
         self.rect.x += self.speedX
         self.rect.y += self.speedY
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(SKYBLUE)
+        self.image = playerImage
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.speedX = 0
         self.speedY = 0
 
     def update(self):
-        self.rect.x += self.speedX
-        self.rect.y += self.speedY
+        if mouseHeld:
+            self.rect.center = (mouseX, mouseY)
+
+
+def spawnNewNPC(x, y):
+    newNPC = NPC()
+    newNPC.rect.center = (x,y)
+    newNPC.speedX = random.randint(-10, 10)
+    newNPC.speedY = random.randint(-10, 10)
+    allSprites.add(newNPC)
+    mobGroup.add(newNPC)
+
 
 
 HEIGHT = 350
@@ -64,8 +82,12 @@ PASTELGREEN = (123, 255, 123)
 pygame.init()
 pygame.mixer.init()
 
+
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(title)
+
+playerImage = pygame.image.load(os.path.join(imgFolder, "sprites/player/bubblegum.png")).convert()
 
 clock = pygame.time.Clock()
 running = True
@@ -83,35 +105,18 @@ mobGroup.add(npc)
 
 while running:
     clock.tick(FPS)
+    mouseX, mouseY = pygame.mouse.get_pos()
+    # print(mouseX)
+    # print(mouseY)
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.speedX = -5
-            if event.key == pygame.K_RIGHT:
-                player.speedX = 5
-            if event.key == pygame.K_UP:
-                player.speedY = -5
-            if event.key == pygame.K_DOWN:
-                player.speedY = 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                player.speedX = -0
-            if event.key == pygame.K_RIGHT:
-                player.speedX = 0
-            if event.key == pygame.K_UP:
-                player.speedY = -0
-            if event.key == pygame.K_DOWN:
-                player.speedY = 0
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x = pygame.mouse.get_pressed(3)
+            if x[0] and player.rect.collidepoint(pygame.mouse.get_pos()):
+                mouseHeld = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouseHeld = False
+            spawnNewNPC(mouseX, mouseY)
 
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_LEFT:
-        #         player.rect.x -= 50
-        #     if event.key == pygame.K_RIGHT:
-        #         player.rect.x += 50
-        #     if event.key == pygame.K_UP:
-        #         player.rect.y -= 50
-        #     if event.key == pygame.K_DOWN:
-        #         player.rect.y += 50
         if event.type == pygame.QUIT:
             running = False
 
